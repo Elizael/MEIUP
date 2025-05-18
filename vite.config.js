fe6cda7d94f10677ea603f8961dab2a2da2c1193
Elizael <55325655+Elizael@users.noreply.github.com>
2025-05-18 15:38:13 -0300

    Ajutes tela branca 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './', // <- ADICIONE ESTA LINHA
+  base: './', // MUITO IMPORTANTE
   plugins: [
     react(),
     VitePWA({
